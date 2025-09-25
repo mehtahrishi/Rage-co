c0408a91fb174acb0665f3448da155508b913199
Hrishi Mehta <mehtahrishi45@gmail.com>
2025-09-25 12:01:17 +0000

    image top view is going behind the navbar i dont want like this in lapto

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -75,7 +75,7 @@ export function SiteHeader() {
   const showTextLogo = !isHomePage || isScrolled;
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="w-full border-b bg-background">
       <div className="container flex h-16 items-center">
         <div className="mr-4 hidden md:flex">
           <Link
