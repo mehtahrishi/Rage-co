1287ed6e07b1ba8d80995b2a6e649c6bdc5a1a57
Hrishi <mehtahrishi45@gmail.com>
2025-11-25 23:30:02 +0530

    quote

--- src/components/animated-quote.tsx
+++ src/components/animated-quote.tsx
@@ -61,7 +61,7 @@ export function AnimatedQuote() {
   return (
     <div ref={ref} className="container mx-auto px-4 pb-8 text-center">
       <svg
-        viewBox="0 0 1200 100"
+        viewBox="0 0 800 80"
         xmlns="http://www.w3.org/2000/svg"
         className="w-full h-auto"
       >
@@ -76,13 +76,13 @@ export function AnimatedQuote() {
         {isInView && (
           <>
             <Star x={50} y={5} custom={1} />
-            <Star x={1150} y={70} custom={2} />
-            <Star x={250} y={85} custom={3} />
-            <Star x={950} y={10} custom={4} />
+            <Star x={750} y={60} custom={2} />
+            <Star x={250} y={65} custom={3} />
+            <Star x={650} y={10} custom={4} />
             <Star x={400} y={15} custom={5} />
-            <Star x={800} y={75} custom={6} />
-            <Star x={100} y={90} custom={7} />
-            <Star x={1050} y={30} custom={8} />
+            <Star x={550} y={70} custom={6} />
+            <Star x={100} y={70} custom={7} />
+            <Star x={700} y={30} custom={8} />
           </>
         )}
 
@@ -94,7 +94,7 @@ export function AnimatedQuote() {
           className="fill-current text-foreground"
           style={{
             fontFamily: "'UnifrakturMaguntia', cursive",
-            fontSize: 'clamp(32px, 5vw, 48px)'
+            fontSize: '51px'
           }}
           variants={containerVariants}
           initial="hidden"
